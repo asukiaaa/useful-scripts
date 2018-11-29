bba5e5ec19614790cd846fd207b23473bc19bc78
Asuk Kono <asukiaaa@gmail.com>
2018-11-29 10:36:16 +0900

    Require project name to rename-kicad-project

--- rename-kicad-project.sh
+++ rename-kicad-project.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-FROM=original_project_name
-TO=new_project_name
+if [[ $1 = "" || $2 = "" ]]; then
+  echo "Please execute like this.
+$0 current_project_name new_project_name"
+  exit 1
+fi
+
+FROM=$1
+TO=$2
 
 FILES=`find . -type f -not -iwholename '*.git/*' | grep $FROM`
 for file in $FILES
